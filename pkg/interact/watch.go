a574dc3859dd865391bdfd3f79a47a15908d4a0b
andreyod <andreyo@il.ibm.com>
2023-01-23 11:54:22 +0200

    Watch clean up.

--- pkg/interact/watch.go
+++ pkg/interact/watch.go
@@ -32,7 +32,6 @@ func Watch(ctx context.Context, cfg *config.Setup, client *kubernetes.Clientset)
 	go metrics.RegisterWatchMetrics()
 
 	// start the threads that will generate events
-	//updateForWatching(ctx, im, cfg.Watch, wg)
 	operations := make(chan string)
 	for i := 0; i < cfg.Parallelism; i++ {
 		wg.Add(1)
@@ -90,18 +89,6 @@ func watchObjects(ctx context.Context, cfg *config.Setup, wg *sync.WaitGroup, ti
 	}()
 }
 
-//func watchObjects(ctx context.Context, cfg *config.Setup, wg *sync.WaitGroup, client *kubernetes.Clientset) {
-//	for i := 0; i < cfg.Parallelism; i++ {
-//		wg.Add(1)
-//		go func() {
-//			defer wg.Done()
-//			if err := watch(ctx, client); err != nil && !errors.Is(err, context.DeadlineExceeded) {
-//				log.WithError(err).Error("failed to interact with the API server")
-//			}
-//		}()
-//	}
-//}
-
 func watch(ctx context.Context, cfg *config.Setup, times map[string][]time.Time, client *kubernetes.Clientset, ns string, mux *sync.RWMutex) error {
 	watcher, err := client.CoreV1().ConfigMaps(ns).Watch(ctx, metav1.ListOptions{})
 	if err != nil {
@@ -114,10 +101,8 @@ func watch(ctx context.Context, cfg *config.Setup, times map[string][]time.Time,
 			return ctx.Err()
 		case event, ok := <-watcher.ResultChan():
 			recieved := time.Now()
-			// i.counterLock.Lock()
-			// i.watchCounter++
-			// i.counterLock.Unlock()
 			if !ok {
+				log.Warning("Watcher channel is closed")
 				return nil
 			}
 			switch event.Type {
@@ -138,80 +123,24 @@ func watch(ctx context.Context, cfg *config.Setup, times map[string][]time.Time,
 			}
 
 			mux.RLock()
-			if val, ok := times[obj.GetName()]; ok {
+			val, ok := times[obj.GetName()]
+			mux.RUnlock()
+			if !ok {
+				//we got the event before (request returned)/(map values set)
+				metrics.TimePoints.WithLabelValues("request-sending").Set(0)
+				metrics.TimePoints.WithLabelValues("request-returned").Set(0)
+				metrics.TimePoints.WithLabelValues("event-recieved").Set(0)
+			} else {
 				metrics.TimePoints.WithLabelValues("request-sending").Set(float64(val[0].UnixMilli()))
 				metrics.TimePoints.WithLabelValues("request-returned").Set(float64(val[1].UnixMilli()))
 				metrics.TimePoints.WithLabelValues("event-recieved").Set(float64(recieved.UnixMilli()))
-			} else {
-				log.Errorf("failed to get times for: %s", obj.GetName())
 			}
-			mux.RUnlock()
 
 			counter++
 			if counter == cfg.ObjectCount {
 				log.Infof("%d update events recieved", counter)
 				return nil
 			}
-
-			// obj, ok := event.Object.(metav1.Object)
-			// if !ok {
-			// 	return fmt.Errorf("expected a metav1.Object in watch, got %T", event.Object)
-			// }
-			// i.timestampsLock.RLock()
-			// timestamp, ok := i.timestampsByRevision[obj.GetResourceVersion()]
-			// i.timestampsLock.RUnlock()
-			// if !ok {
-			// 	continue
-			// }
-
-			// select {
-			// case i.metricsChan <- durationDatapoint{method: "watch", duration: recieved.Sub(timestamp)}:
-			// case <-ctx.Done():
-			// }
 		}
 	}
 }
-
-// func updateForWatching(ctx context.Context, cfg *config.Setup, wg *sync.WaitGroup) {
-// 	for i := 0; i < cfg.Parallelism; i++ {
-// 		wg.Add(1)
-// 		go func() {
-// 			defer wg.Done()
-// 			for {
-// 				select {
-// 				case <-ctx.Done():
-// 					return
-// 				default:
-// 				}
-// 				if err := update(ctx); err != nil && !errors.Is(err, context.DeadlineExceeded) {
-// 					log.WithError(err).Error("failed to interact with the API server")
-// 				}
-// 			}
-// 		}()
-// 	}
-// }
-
-// func update(ctx context.Context) error {
-// 	i.existingLock.Lock()
-// 	if len(i.existing) == 0 {
-// 		i.existingLock.Unlock()
-// 		return i.create(ctx)
-// 	}
-// 	idx := i.r.Intn(len(i.existing))
-// 	key := i.existing[idx]
-// 	counter := i.count[key.namespace]
-// 	i.existingLock.Unlock()
-// 	before := time.Now()
-// 	log.Infof("will run update at: %v", before)
-// 	rv, err := i.d.update(ctx, key.namespace, key.name, types.MergePatchType, []byte(fmt.Sprintf(`{"metadata":{"labels":{"counter":"%d"}}}`, counter)))
-// 	after := time.Now()
-// 	duration := after.Sub(before)
-// 	i.timestampsLock.Lock()
-// 	i.timestampsByRevision[rv] = after
-// 	i.timestampsLock.Unlock()
-// 	select {
-// 	case i.metricsChan <- durationDatapoint{method: "update", duration: duration}:
-// 	case <-ctx.Done():
-// 	}
-// 	return err
-// }
