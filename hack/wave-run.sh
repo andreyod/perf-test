cc33aa0009233577372da32b30692b764a334677
Andrey Odarenko <andreyo@il.ibm.com>
2023-01-31 16:17:56 +0200

    Update wave-run.sh

--- hack/wave-run.sh
+++ hack/wave-run.sh
@@ -5,20 +5,18 @@ go run cmd/main.go
 sleep 15m
 
 #create wave2
-sed -i 's/test-/second/g' config/config
+sed -i 's/"object_name_prefix": "test-"/"object_name_prefix": "second"/g' config/config
 go run cmd/main.go
 sleep 15m
 
 #create wave3
-sed -i 's/second/third/g' config/config
+sed -i 's/"object_name_prefix": "second"/"object_name_prefix": "third"/g' config/config
 go run cmd/main.go
-sed -i 's/third/test-/g' config/config
 sleep 25m
 
 echo "Delete all -----------"
 go run cmd/main.go
-sed -i 's/test-/second/g' config/config
+sed -i 's/"object_name_prefix": "third"/"object_name_prefix": "second"/g' config/config
 go run cmd/main.go
-sed -i 's/second/third/g' config/config
+sed -i 's/"object_name_prefix": "second"/"object_name_prefix": "test-"/g' config/config
 go run cmd/main.go
-sed -i 's/third/test-/g' config/config
